ad78cd9f09dbd4f88fc5e248f9a47c2f20b0305c
Jayson A. Eballena <jae@iliganlight.com>
2026-02-20 20:39:50 +0800

    Refactor: Added retrofit implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,4 +76,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
 
     implementation("com.google.maps.android:maps-compose:4.3.0")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 }
